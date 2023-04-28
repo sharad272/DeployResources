72f7eebd800e5a2f4eae27b37e9fc7da00bbec60
sharad272 <63502537+sharad272@users.noreply.github.com>
2023-04-29 00:49:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ resource "azurerm_app_service" "web_app" {
   app_service_plan_id = "/subscriptions/f545f248-4fb2-4f9c-a0cd-190c842b66d8/resourceGroups/research_development/providers/Microsoft.Web/serverfarms/ASP-researchdevelopment-b0ec"
 
   site_config {
-    dotnet_framework_version = "v7.0"
+    dotnet_framework_version = "v6.0"
   }
 }
