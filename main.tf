36a238ae3126e43adfe8afbc9410b652662156d6
sharad272 <63502537+sharad272@users.noreply.github.com>
2023-04-29 00:20:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ variable "web_app_names" {
 }
 
 # Create a resource for each web app using count.index
-resource "azurerm_linux_web_app" "web_app" {
+resource "azurerm_windows_web_app" "web_app" {
   count = length(var.web_app_names)
 #  name                = "${var.web_app_names[count.index]}-app-service"
   name                = var.web_app_names[count.index]
   location            = "eastus"
   resource_group_name = "research_development"
-  app_service_plan_id = "/subscriptions/f545f248-4fb2-4f9c-a0cd-190c842b66d8/resourceGroups/research_development/providers/Microsoft.Web/serverfarms/ASP-researchdevelopment-b0ec"
+  service_plan_id = "/subscriptions/f545f248-4fb2-4f9c-a0cd-190c842b66d8/resourceGroups/research_development/providers/Microsoft.Web/serverfarms/ASP-researchdevelopment-b0ec"
 
   site_config {
     dotnet_framework_version = "v5.0"
